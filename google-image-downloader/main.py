6c94facbd976c83565c7ac99a98868a22d84b298
植田佳和侑 <kanau20@icloud.com>
2026-03-27 20:49:07 +0900

    feat:local-or-HDD

--- google-image-downloader/main.py
+++ google-image-downloader/main.py
@@ -2,13 +2,16 @@ from pathlib import Path
 from util.tiles_coordinate_calculator import get_tiles_in_city
 from util.google_map_tiles_downloader import GoogleMapTilesDownloader
 
+# OUTPUT_ROOT = Path("/Volumes/HDPH-UTV/google-satellite-image")
+OUTPUT_ROOT = Path(__file__).parent / "output"
+
 def main():
-    city_name = "長生村"
+    city_name = "白井市"
     zoom_level = 20
     
     tiles = get_tiles_in_city(city_name, zoom_level)
 
-    output_dir = Path(__file__).parent / f"output_{city_name}"
+    output_dir = OUTPUT_ROOT / f"{city_name}_downloading"
     downloader = GoogleMapTilesDownloader(str(output_dir))
     
     downloader.download_tiles(tiles, zoom_level)
